13377859eb000ae03982f3d0758af3897d7d2079
Dragan Djuric <dragan@dragandjuric.com>
2013-05-28 11:32:53 +0200

    Added scm to project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject uncomplicate/fluokitten "0.1.0-SNAPSHOT"
   :description "Category theory concepts in Clojure - Functors, Applicatives, Monads, Monoids and more."
   :url "https://github.com/uncomplicate/fluokitten"
+  :scm {:name "git"
+         :url "http://www.eclipse.org/legal/epl-v10.html"})
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[codox "0.6.4"]]
   :codox {:src-dir-uri "http://github.com/uncomplicate/fluokitten/blob/master"
           :src-linenum-anchor-prefix "L"
           :exclude [uncomplicate.fluokitten.algo]
-          :output-dir "doc/codox"}
+          :output-dir "docs/codox"}
   :profiles {:dev {:dependencies [[midje "1.5.0"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+                   :plugins [[lein-midje "3.0.0"]
+                             [codox "0.6.4"]
+                             [lein-marginalia "0.7.1"]]}})
