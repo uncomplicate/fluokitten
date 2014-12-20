5b3dff05a0b7e1a986530b4dc4f6cdfe87c45d6f
Alex Davydow <adavydow@gmail.com>
2014-12-20 04:36:08 +0300

    changelog for 0.4.0-adavydow

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uncomplicate/fluokitten "0.3.1-SNAPSHOT"
+(defproject uncomplicate/fluokitten "0.4.0-adavydow-SNAPSHOT"
   :description "Category theory concepts in Clojure - Functors, Applicatives, Monads, Monoids and more."
   :url "https://github.com/uncomplicate/fluokitten"
   :scm {:name "git"
