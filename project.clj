47126ff84e5f7396fa7c37d6e2f2a8d250c5c0a0
Dragan Djuric <dragandj@gmail.com>
2016-03-11 01:38:05 +0100

    Changed version to SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uncomplicate/fluokitten "0.4.0"
+(defproject uncomplicate/fluokitten "0.5.0-SNAPSHOT"
   :description "Category theory concepts in Clojure - Functors, Applicatives, Monads, Monoids and more."
   :url "https://github.com/uncomplicate/fluokitten"
   :scm {:name "git"
